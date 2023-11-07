f88144f16dbb255484d40cf9d442eac0bcf26e2d
Lalo <laurencioberro@hotmail.com>
2023-11-07 19:39:14 -0300

    [F] Fixed RealtimeTimerExecutor Instalation

--- Runtime/RealtimeTimer/Installers/Zenject/RealtimeTimerExecutorZinstaller.cs
+++ Runtime/RealtimeTimer/Installers/Zenject/RealtimeTimerExecutorZinstaller.cs
@@ -1,20 +1,23 @@
 ﻿using ServiceLocatorPattern;
 using Timer.Runtime.Realtime.Domain;
+using UnityEngine;
 using ZenjectExtensions.Zinstallers;
 
 namespace Timer.Runtime.Realtime.Installers
 {
     public class RealtimeTimerExecutorZinstaller : Zinstaller
     {
+        [Header("References")] 
+        [SerializeField] private RealtimeTimerExecutor _realtimeTimerExecutor;
+        
         public override void Install()
         {
             if (ServiceLocator.Instance.IsContained<IRealtimeTimerExecutor>())
                 return;
             
-            IRealtimeTimerExecutor realtimeTimerExecutor = new RealtimeTimerExecutor();
-            realtimeTimerExecutor.Initialize();
+            _realtimeTimerExecutor.Initialize();
             
-            ServiceLocator.Instance.Register(realtimeTimerExecutor);
+            ServiceLocator.Instance.Register<IRealtimeTimerExecutor>(_realtimeTimerExecutor);
         }
     }
 }
